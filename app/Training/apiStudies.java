bc0379eb167694ebdc303bdbff5c9819a003d6f0
David Zemlin <dzemlin@asu.edu>
2023-10-10 20:54:36 -0400

    Notes from API Studies

---
+++ app/Training/apiStudies.java
@@ -0,0 +1,23 @@
+/*
+What does "API" stand for? 
+	Application programing interface
+
+What does an API do?
+	It is an interface between 2 programs, so that the operator know what commands they can send and what they can get back
+	much like an interface in java
+
+How do I know when calls I can make to the API?
+
+Is our API separated from the backend? or are they the same program?
+
+Where is the API in our program?
+
+What Are Endpoints?
+
+How do I know what form to send a request in?
+
+How do I send requests to the API?
+
+Does the API need to be imported into the front end program or something?
+
+*/
