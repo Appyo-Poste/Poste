6b251d88dfc1e2bef26d7d9f9a52218f70cef8b2
David McConnell <mccodav1@gmail.com>
2023-11-25 12:30:10 -0500

    Fix sharing and unsharing

--- app/src/main/java/com/example/poste/activities/FolderUserActivity.java
+++ app/src/main/java/com/example/poste/activities/FolderUserActivity.java
@@ -10,13 +10,23 @@ import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.poste.R;
 import com.example.poste.adapters.UserAdapter;
+import com.example.poste.http.MyApiService;
+import com.example.poste.http.RetrofitClient;
+import com.example.poste.http.UpdateFolderPermissionsRequest;
 import com.example.poste.models.Folder;
+import com.example.poste.models.FolderAccess;
 import com.example.poste.models.User;
 
 import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class FolderUserActivity extends AppCompatActivity {
 
@@ -51,6 +61,31 @@ public class FolderUserActivity extends AppCompatActivity {
             public void onItemClick(int position, String user) {
                 Log.d("UserDebug", "Clicked: User " + (position + 1) + ": " + user);
                 // unshare folder with user
+                // get the email associated with this recycler view item
+                String email = sharedUsers.get(position);
+                MyApiService apiService = RetrofitClient.getRetrofitInstance().create(MyApiService.class);
+                Call<ResponseBody> call = apiService.updateFolderPermissions(
+                        User.getUser().getTokenHeaderString(),
+                        new UpdateFolderPermissionsRequest(folderID, email, FolderAccess.NONE));
+
+                call.enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        if (response.isSuccessful()){
+                            Toast.makeText(FolderUserActivity.this,"Unshared folder with " + email, Toast.LENGTH_SHORT).show();
+                            sharedUsers.remove(position);
+                            recyclerView.getAdapter().notifyItemRemoved(position);
+                            recyclerView.getAdapter().notifyItemRangeChanged(position, sharedUsers.size());
+                        } else {
+                            Toast.makeText(FolderUserActivity.this, "Error: " + response.message(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        Toast.makeText(FolderUserActivity.this, "Unable to communicate with server", Toast.LENGTH_SHORT).show();
+                    }
+                });
 
             }
 
