8831a05da0b5055dfa832eb8d290a06b947360ee
Fayez Mehdad <fmehdad@asu.edu>
2024-03-25 00:25:19 +0600

    Single share composable object (without dropdown to change share level)

--- app/src/main/java/com/poste/reusables/ShareFolderDialog.kt
+++ app/src/main/java/com/poste/reusables/ShareFolderDialog.kt
@@ -1,28 +1,28 @@
 package com.poste.reusables
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -55,9 +55,6 @@ fun ShareFolderDialog(
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-
-                    var folderName by remember { mutableStateOf(folder.title) }
-
                     Spacer(modifier = Modifier.height(16.dp))
 
                     Text(
@@ -143,4 +140,66 @@ fun ShareFolderDialog(
             }
         }
     }
+}
+
+@Preview
+    (showBackground = true)
+@Composable
+fun PreviewShareItemComposable(){
+    PosteTheme {
+        Column {
+            ShareItemComposable(
+                emailSharedWith = "jason@gmail.com",
+                sharelevel = "Can view"
+            )
+            ShareItemComposable(
+                emailSharedWith = "davidcookoatmeal@email.com",
+                sharelevel = "Can view"
+            )
+        }
+    }
+}
+
+@Composable
+fun ShareItemComposable(
+    emailSharedWith: String,
+    sharelevel: String){
+    Box(
+        modifier = Modifier
+            .background(Color.Gray),
+        contentAlignment = Alignment.Center
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .padding(5.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ){
+            Text(
+                text = emailSharedWith,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 5.dp),
+                style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.Bold)
+            )
+            Text(
+                text = sharelevel,
+                style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.Normal)
+            )
+            Spacer(modifier = Modifier.width(20.dp))
+            Image(
+                painter = painterResource(R.drawable.close),
+                contentDescription = "Icon for delete share",
+                modifier = Modifier
+                    .size(18.dp)
+                    .clickable {
+                        /*TODO*/
+                    }
+                    .padding(end = 5.dp),
+            )
+        }
+    }
+
 }
