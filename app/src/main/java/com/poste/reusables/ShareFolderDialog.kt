0395ce391cde75b8858c2a7976a5e2fcfa355d86
Fayez Mehdad <fmehdad@asu.edu>
2024-03-26 13:42:08 +0600

    Rework drop down menu according to Material3

--- app/src/main/java/com/poste/reusables/ShareFolderDialog.kt
+++ app/src/main/java/com/poste/reusables/ShareFolderDialog.kt
@@ -12,11 +12,21 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -164,9 +174,14 @@ fun PreviewShareItemComposable(){
 fun ShareItemComposable(
     emailSharedWith: String,
     sharelevel: String){
+
+
+    var dropdownExpanded by rememberSaveable { mutableStateOf(false) }
+    var mutableShareLevel by rememberSaveable { mutableStateOf(sharelevel) }
+
     Box(
         modifier = Modifier
-            .background(Color.Gray),
+            .background(Color.LightGray),
         contentAlignment = Alignment.Center
     ){
         Row(
@@ -184,10 +199,60 @@ fun ShareItemComposable(
                     .padding(start = 5.dp),
                 style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.Bold)
             )
-            Text(
-                text = sharelevel,
-                style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.Normal)
-            )
+            Box(
+                modifier = Modifier
+                    .wrapContentSize(Alignment.TopStart),
+                contentAlignment = Alignment.Center
+            ){
+                Box(modifier = Modifier
+                    .clickable {
+                        dropdownExpanded = true
+                    },
+                    contentAlignment = Alignment.Center){
+                    Row(verticalAlignment = Alignment.CenterVertically){
+                        Text(
+                            text = mutableShareLevel,
+                            style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.Normal)
+                        )
+                        Icon(Icons.Default.ExpandMore, contentDescription = "Localized description")
+                    }
+                }
+                DropdownMenu(
+                    expanded = dropdownExpanded,
+                    onDismissRequest = { dropdownExpanded = false }
+                ) {
+                    DropdownMenuItem(
+                        text = { Text("Can View") },
+                        onClick = {
+                            /*TODO: API Request things*/
+                            mutableShareLevel = "Can View"
+                            dropdownExpanded = false
+                        },
+                        )
+                    DropdownMenuItem(
+                        text = { Text("Can Edit") },
+                        onClick = {
+                            /*TODO: API Request things*/
+                            mutableShareLevel = "Can Edit"
+                            dropdownExpanded = false
+                        },
+                        /*leadingIcon = {
+                            Icon(
+                                Icons.Outlined.Edit,
+                                contentDescription = null
+                            )
+                        }*/
+                        )
+                    DropdownMenuItem(
+                        text = { Text("Full Access") },
+                        onClick = {
+                            /*TODO: API Request things*/
+                            mutableShareLevel = "Full Access"
+                            dropdownExpanded = false
+                        },
+                        )
+                }
+            }
             Spacer(modifier = Modifier.width(20.dp))
             Image(
                 painter = painterResource(R.drawable.close),
