81a418f42d38da03b8e397ebbca43a8a0106aa84
Fayez Mehdad <fmehdad@asu.edu>
2024-03-28 05:36:25 +0600

    Update FolderComposable UI to match prototype

--- app/src/main/java/com/poste/reusables/FolderComposable.kt
+++ app/src/main/java/com/poste/reusables/FolderComposable.kt
@@ -1,13 +1,12 @@
 package com.poste.reusables
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -15,11 +14,11 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Folder
 import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material.icons.outlined.Share
 import androidx.compose.material.icons.outlined.Upload
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -29,9 +28,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.myapplication.R
 import com.poste.models.Folder
 import com.poste.ui.theme.PosteTheme
 
@@ -48,7 +51,6 @@ private fun FolderComposable(folder: Folder) {
         modifier = Modifier
             .padding(all = 8.dp)
             .fillMaxWidth()
-            .height(80.dp)
             .padding(start = 7.5.dp, end = 7.5.dp)
             .clickable {
                 /* TODO: add click functionality */
@@ -56,61 +58,75 @@ private fun FolderComposable(folder: Folder) {
         horizontalArrangement = Arrangement.Start,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Box(
-            Modifier.size(55.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Icon(Icons.Default.Folder, contentDescription = "Icon for folder",
+        Column(modifier = Modifier.weight(0.5f), horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = painterResource(R.drawable.folder),
+                contentDescription = "Poste Icon",
                 modifier = Modifier
-                    .size(55.dp)
+                    .size(45.dp)
+
             )
-        }
-
-        Spacer(modifier = Modifier.width(8.dp))
-
-        Box(Modifier.size(192.dp), contentAlignment = Alignment.Center) {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Box(Modifier.size(90.dp),contentAlignment = Alignment.Center) {
-                    Column {
-                        Text(text = folder.title, maxLines = 2, overflow = TextOverflow.Ellipsis)
-                        Text(text = "${folder.file_count} file" + if (folder.file_count != 1) "s" else "")
-                    }
-                }
-
-                Spacer(modifier = Modifier.width(6.dp))
-
-                Box(contentAlignment = Alignment.Center) {
-                    Text(
-                        text = folder.getShortCreationDate()
+            if (folder.file_count != 0) {
+                Text(text = "${folder.file_count} file${if (folder.file_count != 1) "s" else ""}",
+                    style = MaterialTheme.typography.labelSmall
                     )
-                }
+            }
+            if (folder.folder_count != 0) {
+                Text(text = "${folder.folder_count} folder${if (folder.folder_count != 1) "s" else ""}",
+                    style = MaterialTheme.typography.labelSmall
+                )
+            }
+            if (folder.file_count == 0 && folder.folder_count == 0){
+                Text(
+                    text = "Empty \nfolder!",
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.labelSmall
+                )
             }
         }
-        Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
-            Row {
-                Icon(Icons.Outlined.Edit, contentDescription = "Icon for edit",
-                    modifier = Modifier
-                        .size(30.dp)
-                        .clip(RoundedCornerShape(15.dp))
-                        .clickable { showEditFolderDialog = true }
-                )
-                Icon(Icons.Outlined.Upload, contentDescription = "Icon for move folder",
-                    modifier = Modifier
-                        .size(30.dp)
-                        .clip(RoundedCornerShape(15.dp))
-                        .clickable {
-                                   /* TODO: add click functionality for move folder */
-                                   }
-                )
-                Icon(Icons.Outlined.Share, contentDescription = "Icon for share",
-                    modifier = Modifier
-                        .size(30.dp)
-                        .clip(RoundedCornerShape(15.dp))
-                        .clickable {
-                            showShareFolderDialog = true
-                        }
-                )
-            }
+
+        Spacer(modifier = Modifier.width(10.dp))
+
+        Text(
+            text = folder.title,
+            style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Medium),
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.weight(1f)
+        )
+
+
+        Spacer(modifier = Modifier.width(6.dp))
+
+        Text(
+            text = folder.getShortCreationDate(),
+            Modifier.padding(end = 6.dp),
+            style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Medium),
+            )
+
+        Row {
+            Icon(Icons.Outlined.Edit, contentDescription = "Icon for edit",
+                modifier = Modifier
+                    .size(30.dp)
+                    .clip(RoundedCornerShape(15.dp))
+                    .clickable { showEditFolderDialog = true }
+            )
+            Icon(Icons.Outlined.Upload, contentDescription = "Icon for move folder",
+                modifier = Modifier
+                    .size(30.dp)
+                    .clip(RoundedCornerShape(15.dp))
+                    .clickable {
+                        /* TODO: add click functionality for move folder */
+                    }
+            )
+            Icon(Icons.Outlined.Share, contentDescription = "Icon for share",
+                modifier = Modifier
+                    .size(30.dp)
+                    .clip(RoundedCornerShape(15.dp))
+                    .clickable {
+                        showShareFolderDialog = true
+                    }
+            )
         }
     }
 }
